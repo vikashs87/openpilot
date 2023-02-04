0467338d6b12e139fbffde3fdb0022f21d9642ae
Comma Device <device@comma.ai>
2023-02-03 23:20:55 -0800

    add features

--- gm_send.py
+++ gm_send.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import sys
 import time
 import argparse
@@ -9,7 +10,7 @@ from opendbc.can.packer import CANPacker
 from selfdrive.car.gm.gmcan import create_steering_control
 
 panda = Panda()
-panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT, 1)
 packer = CANPacker("gm_global_a_powertrain_generated")
 
 DT_CTRL = 0.01  # 100Hz
@@ -21,9 +22,14 @@ idx = 0
 last_steer_time = 0
 last_param_read_time = 0
 last_can_recv_time = 0
+last_send_one_fast = True
+
+temp_send_msgs = 0
+temp_send_rate = (1 / 1000)  # 3ms faults, 4ms faults, 5ms took 4 to fault, testing 6ms
+
 while True:
   now = time.perf_counter()
-  if (now - last_param_read_time) > 1:  # 1 second
+  if (now - last_param_read_time) > 3:  # 1 second
     last_param_read_time = now
     try:
       with open('/data/send_rate', 'r') as f:
@@ -31,12 +37,31 @@ while True:
     except:
       pass
 
-  if (now - last_steer_time) > send_rate:
+    try:
+      with open('/data/temp_send_rate', 'r') as f:
+        temp_send_rate = float(f.read().strip())
+    except:
+      pass
+
+  _send_rate = send_rate
+  if temp_send_msgs != 0:
+    _send_rate = temp_send_rate
+  if (now - last_steer_time) > _send_rate:
+    temp_send_msgs = max(0, temp_send_msgs - 1)
     last_steer_time = now
     counter = idx % 4
     addr, _, dat, bus = create_steering_control(packer, bus=0, apply_steer=0, idx=counter, lkas_active=False)
     panda.can_send(addr, dat, bus)
     idx += 1
+    send_one_fast = os.path.exists('/data/send_one_fast')
+    if send_one_fast and not last_send_one_fast:
+      temp_send_msgs = 1
+      #time.sleep(3 / 1000)
+      #counter = idx % 4
+      #addr, _, dat, bus = create_steering_control(packer, bus=0, apply_steer=0, idx=counter, lkas_active=False)
+      #panda.can_send(addr, dat, bus)
+      #idx += 1
+    last_send_one_fast = send_one_fast
 
   if (now - last_can_recv_time) > recv_rate:
     last_can_recv_time = now
@@ -45,4 +70,4 @@ while True:
       if address == 388 and bus == 0:
         torque_delivered_status = (dat[0] >> 3) & 0x7
         if torque_delivered_status == 3:
-          print("LKAS FAULT!")
+          print("LKAS FAULT! SENT:", idx)
