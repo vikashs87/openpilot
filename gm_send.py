9b1091513422cb7ee963436557e0acf15aac5798
Shane Smiskol <shane@smiskol.com>
2023-02-03 20:36:25 -0800

    more accurate

--- gm_send.py
+++ gm_send.py
@@ -12,13 +12,24 @@ panda = Panda()
 panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
 packer = CANPacker("gm_global_a_powertrain_generated")
 
-DT_CTRL = 0.01
+DT_CTRL = 0.01  # 100Hz
+send_rate = DT_CTRL
 
 idx = 0
+last_steer_time = 0
+last_param_read_time = 0
 while True:
-  counter = idx % 4
-  addr, _, dat, bus = create_steering_control(packer, bus=0, apply_steer=0, idx=counter, lkas_active=False)
-  panda.can_send(addr, dat, bus)
-  idx += 1
+  now = time.perf_counter()
+  if (now - last_param_read_time) > 1:  # 1 second
+    try:
+      with open('/data/send_rate', 'r') as f:
+        send_rate = float(f.read().strip())
+    except:
+      pass
 
-  time.sleep(DT_CTRL)
+  if (now - last_steer_time) > send_rate:
+    last_steer_time = now
+    counter = idx % 4
+    addr, _, dat, bus = create_steering_control(packer, bus=0, apply_steer=0, idx=counter, lkas_active=False)
+    panda.can_send(addr, dat, bus)
+    idx += 1
